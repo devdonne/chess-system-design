319617f3c66c82712ad788963c815bfa7508bd99
Daniel Araújo <dannielaraujooficial@gmail.com>
2023-07-04 20:55:48 -0300

    move pieces

--- ChessSystemJava/src/application/Run.java
+++ ChessSystemJava/src/application/Run.java
@@ -1,11 +1,28 @@
 package application;
 
-import chess.ChessMatch;
+import chess.*;
+import java.util.Scanner;
 
 public class Run {
     public static void main(String args[]){
         ChessMatch chessMatch = new ChessMatch();
         
-        UI.printBoard(chessMatch.getPieces());
+        while (true){
+            UI.printBoard(chessMatch.getPieces());
+            
+            System.out.println("");
+            
+            System.out.println("Source:");
+            ChessPosition source = UI.readChessPosition(new Scanner(System.in));
+            
+            System.out.println("");
+            System.out.println("Target: ");
+            
+            ChessPosition target = UI.readChessPosition(new Scanner(System.in));
+            
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+        
+        
     }
 }
