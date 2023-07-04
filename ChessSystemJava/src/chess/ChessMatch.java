cc3eab6a734d1e8758fa16bebf27a6963505e011
Daniel Araújo <dannielaraujooficial@gmail.com>
2023-07-04 16:48:02 -0300

    place pieces on the board

--- ChessSystemJava/src/chess/ChessMatch.java
+++ ChessSystemJava/src/chess/ChessMatch.java
@@ -1,12 +1,14 @@
 package chess;
 
-import boardgame.Board;
+import boardgame.*;
+import chess.pieces.*;
 
 public class ChessMatch {
     private Board board;
     
     public ChessMatch(){
         board = new Board(8,8);
+        inicialSetup();
     }
     
     public ChessPiece[][] getPieces(){
@@ -18,5 +20,11 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+    
+    private void inicialSetup(){
+        board.placePiece(new Rook(board, Color.WHITE), new Position(0,0));
+        board.placePiece(new King(board, Color.WHITE), new Position(0,4));
+         board.placePiece(new King(board, Color.WHITE), new Position(7,4));
     }
 }
